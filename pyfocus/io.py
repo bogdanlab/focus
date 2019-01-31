44b41c28449c4ae71ebeb75f4a420803488d8f8f
quattro <nick.mancuso@gmail.com>
2019-01-30 16:17:27 -0800

    Updated munge function.

--- pyfocus/io.py
+++ pyfocus/io.py
@@ -3,15 +3,12 @@ __all__ = ["is_file", "write_output"]
 
 def is_file(f):
     """
-    Check if f is a file-like object. Compatible with Python 2 and 3
-
-    See https://stackoverflow.com/questions/47518707/check-if-an-object-is-a-file-in-python-2-and-3
+    Check if f is a file-like object. Compatible with Python 3
 
     :param f:
     :return: True if `f` is file-like; False otherwise.
     """
-    import sys
-    return isinstance(f, file) if sys.version_info[0] == 2 else hasattr(f, 'read')
+    return hasattr(f, 'read')
 
 
 def write_output(results, output, append=False):
